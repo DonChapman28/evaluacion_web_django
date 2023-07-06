0b7e5dc66d83b3d80e7cc4d32dde345e48fa8e81
eSponjosa <105675474+DonChapman28@users.noreply.github.com>
2023-07-06 16:23:29 -0400

    v0.3
    falta carrito y validaciones

--- mascotas/tienda/forms.py
+++ mascotas/tienda/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Producto,Marca,Categoria
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ProductoForm(forms.ModelForm):
     class Meta:
@@ -18,6 +19,41 @@ class CategoriaForm(forms.ModelForm):
         fields = '__all__'
 
 #usuario
+opciones_region = [
+    [0, "Región de Arica y Parinacota"],
+    [1, "Región de Tarapacá"],
+    [2, "Región de Antofagasta"],
+    [3, "Región de Atacama"],
+    [4, "Región de Coquimbo"],
+    [5, "Región de Valparaíso"],
+    [6, "Región Metropolitana"],
+    [7, "Región de O'Higgins"],
+    [8, "Región del Maule"],
+    [9, "Región del Ñuble"],
+    [10, "Región del Biobío"],
+    [11, "Región de La Araucanía"],
+    [12, "Región de Los Ríos"],
+    [13, "Región de Los Lagos"],
+    [14, "Región de Aysén"],
+    [15, "Región de Magallanes"],
+]
+
+opciones_educacion = [
+    [0, "Magister"],
+    [1, "Doctor"],
+    [2, "Profesional"],
+    [3, "Otro"],
+]
+
 class CustomUserCreationForm(UserCreationForm):
-    pass
+   
+    rut = forms.IntegerField()
+    dv = forms.CharField(max_length=1)
+    fecha_Nacimiento = forms.DateField()
+    region = forms.IntegerField(widget=forms.Select(choices=opciones_region))
+    telefono = forms.CharField(max_length=15)
+    nivel_Educacion = forms.IntegerField(widget=forms.Select(choices=opciones_educacion))
+    class Meta:
+        model = User
+        fields = ('rut','dv','first_name','last_name','fecha_Nacimiento','email','region','telefono','nivel_Educacion','username','password1', 'password2')
 
--- mascotas/tienda/models.py
+++ mascotas/tienda/models.py
@@ -29,4 +29,6 @@ class Producto(models.Model):
     def __str__(self):
         return self.nombre
     
+
+    
     
